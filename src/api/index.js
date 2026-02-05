6c977b30be1243594631edc8b5a686ebe04adb83
Ektoras Makras <emakras@draxis.gr>
2026-02-05 11:22:53 +0200

    feat: remove authentication API functions from index export

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,6 @@
 // Export main API functionality
 export { api, apiObject } from "./api.js";
 
-// Export authentication API
-export { loginUser, registerUser, logoutUser } from "./authApi.js";
-
 // Export constants
 export { apiStatus } from "./constants/apiStatus.js";
 
