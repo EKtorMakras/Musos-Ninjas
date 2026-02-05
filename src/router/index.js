53c2a179fefb9d1399f83fcd6760329c66c374eb
Ektoras Makras <emakras@draxis.gr>
2026-02-05 11:28:21 +0200

    feat: implement route guard to redirect authenticated users from login to home

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
+import { useAuthStore } from "@/stores/useAuth";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     // linkExactActiveClass: "text-primary-500",
+});
+
+router.beforeEach((to) => {
+    const authStore = useAuthStore();
+
+    if (to.name === "login" && authStore.userIsAuth) {
+        return { name: "home" };
+    }
 });
 
 export default router;
